8918890b907c13ea06fb57f982a28a1d628673fb
Luís Arthur Turck da Silva <luisarthur0207@gmail.com>
2025-06-17 08:46:14 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -6,3 +6,27 @@ function passar(){
     textoQuestao.textContent= level[i]
     i=i+1
 }
+idx=0
+const pergunta= document.getElementById("pergunta")
+const bo1= document.getElementById("bo1")
+const bo2= document.getElementById("bo2")
+const bo3= document.getElementById("bo3")
+const bo4= document.getElementById("bo4")
+const perguntas=[["Qual foi a região mais atingida nas duas grandes guerras?","Ásia","Europa"," América do Norte","África", 2]]
+document.getElementById("idx").textContent = idx+1
+pergunta.textContent = perguntas[indx][0]
+bo1.textContent = levels[idx][1]
+bo2.textContent = levels[idx][2]
+bo3.textContent = levels[idx][3]
+bo4.textContent = levels[idx][4]
+function respostas(res){
+    if (res == perguntas[idx][5]){
+        indx++;
+        document.getElementById("indx").textContent = indx+1
+        pergunta.textContent = perguntas[idx][0]
+        bo1.textContent = perguntas[idx][1]
+        bo2.textContent = perguntas[idx][2]
+        bo3.textContent = perguntas[idx][3]
+        bo4.textContent = perguntas[idx][4]
+    }
+}
